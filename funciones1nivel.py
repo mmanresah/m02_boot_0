685bd43d3b7100e360c76ad1415e4ee003012580
Miguel Manresa <mmanresah@gmail.com>
2019-05-13 13:29:55 +0200

    Funciones Lambdas"

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,6 +4,9 @@ def normal(x):
 def cuadrado(x):
     return x*x
 
+def cubo(x):
+    return x**3
+
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range(0, limitTo+1):
@@ -12,7 +15,6 @@ def sumaTodos(limitTo, f):
     return resultado
 
 
-
-
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
---
+++ funcionesLambda.py
@@ -0,0 +1,3 @@
+from funciones1nivel import sumaTodos
+
+print(sumaTodos(3, lambda x: x**3))
